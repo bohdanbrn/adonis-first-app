d3aea0fe5888e7418ab6dde5ca6c95ca01a5f44a
bohdanbrn <bohdanbrn@gmail.com>
2019-07-08 00:11:05 +0300

    first commit

---
+++ app/Controllers/Http/PostController.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const Post = use('App/Models/Post')
+
+class PostController {
+    async index({ view }) {
+        const posts = await Post.all()
+
+        return view.render('posts.index', {
+            title: 'Latest posts',
+            posts: posts.toJSON()
+        })
+    }
+
+    async add({ view }) {
+        return view.render('posts.add')
+    }
+
+    async details({ params, view }) {
+        const post = await Post.find(params.id)
+
+        return view.render('posts.details', {
+            post: post
+        })
+    }
+
+}
+
+module.exports = PostController
